e58863f434e05a064bffbd1af3bf711d88513170
Deepak Kumar <deepak.kumar@iphtechnologies.com>
2024-06-13 18:55:15 +0530

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => NumberList2()),
       ],
       child: MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
